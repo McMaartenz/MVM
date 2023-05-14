aa18ae3bcbc3a2241f9ac33feffa426be4db071d
Maarten van Keulen <mcmaartenz@outlook.com>
2023-05-14 19:07:26 +0200

    std::vector out_of_range checking and proper resize

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -1,7 +1,7 @@
 #include "Memory.h"
 
 Memory::Memory(uint32_t memory_size) {
-	data.reserve(memory_size);
+	data.resize(memory_size, 0);
 }
 
 Memory::~Memory() {
@@ -10,12 +10,12 @@ Memory::~Memory() {
 
 uint8_t Memory::get(uint32_t address) {
 	// TODO: Apply MMU logic
-	return data[address];
+	return data.at(address);
 }
 
 void Memory::set(uint32_t address, uint8_t value) {
 	// TODO: Apply MMU logic
-	data[address] = value;
+	data.at(address) = value;
 }
 
 void Memory::write_buffer(uint32_t address, const uint8_t* buffer, uint32_t count) {
